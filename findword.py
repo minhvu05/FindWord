482526797a27830204febc98f707d566d23185e3
minhvu05 <minhvu0514@gmail.com>
2025-02-17 04:37:25 -0500

    last commit

--- findword.py
+++ findword.py
@@ -1,24 +1,42 @@
 import os
 import pandas as pd
 
+
 def find_word_from_end_string(file_name, suffix):
-    # Creating & formatting proper dataframe from given txt file
-    df = pd.read_csv(file_name, sep = ' ', header = None)
+    # Creating proper dataframe from given txt file
+    df = pd.read_csv(file_name, sep = ' ', header = None, on_bad_lines = 'skip')
     output_rows = []
 
-    # piping the count at the end of the row
+    # Piping the count at the end of the row
     for i, row in df.iterrows():
         count = 0
+        # Store all valid words for output row
+        valid_words = []  
         for words in row:
-            if words.endswith(suffix):    
-                count += 1
+            # Skip nan values
+            if pd.notna(words):
+                # Convert word to string to avoid float issues
+                words = str(words)
+                if words.endswith(suffix):    
+                    count += 1
+                # Add valid word to filtered row array
+                valid_words.append(words)  
         if count > 0:
-            output_row = ' '.join(row) + f'|{count}'
+            output_row = ' '.join(valid_words) + f'|{count}'
             output_rows.append(output_row)
             
 
     with open("ans.txt", 'w') as f:
         f.write("\n".join(output_rows))
 
-# error when # of words on each row is different
+
 find_word_from_end_string("file1.txt", "y")
+
+
+
+
+
+# without "on_bad_lines = 'skip'" param, it gives an error?
+# pandas.errors.ParserError: Error tokenizing data. C error: Expected 3 fields in line 3, saw 4
+# no idea how file1 passes with 1st row 6 fields, 2nd row 4 fields
+# but file2 does not pass with 1st row 3 fields, 3rd row 4 fields??
