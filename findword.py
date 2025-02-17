af6af2a74645fe426341457406d03e1aaf2653f9
minhvu05 <minhvu0514@gmail.com>
2025-02-17 04:40:02 -0500

    fix comment & file

--- findword.py
+++ findword.py
@@ -38,9 +38,9 @@ find_word_from_end_string("file4.txt", "e")
 
 # without "on_bad_lines = 'skip'" param, it gives an error?
 # pandas.errors.ParserError: Error tokenizing data. C error: Expected 3 fields in line 3, saw 4
-# no idea how file1 passes with 1st row 6 fields, 2nd row 4 fields
-# but file2 does not pass with 1st row 3 fields, 3rd row 4 fields??
 
+# file1 passes with 1st row 6 fields, 2nd row 4 fields
+# but file2 does not pass with 1st row 3 fields, 3rd row 4 fields
 # probably bc 1st row can have excess columns, but can't make extra columns after init???
 
-# but on_bad_lines = 'skip' removes all rows with more columns than the first row
+# on_bad_lines = 'skip' removes all rows with more columns than the first row
